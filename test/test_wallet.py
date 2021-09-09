15bdaacab957458923f8d36ceb95b20a8691fe24
kundiorro <grzybek2413@gmail.com>
2021-09-09 18:19:38 +0200

    test file

--- test/test_wallet.py
+++ test/test_wallet.py
@@ -1,9 +1,26 @@
+from portfel import wallet
 
-from app import wallet
 
+def test_get_value_in_pln(mocker):
+    mocker.patch(
+        "portfel.requester.ExchangeRateRequester.get_todays_rate", return_value=4.50
+    )
 
-def test_get_value_in_pln():
     wallet_ = wallet.Wallet()
     result = wallet_.get_value_in_pln()
 
     assert result == 0
+
+
+def test_get_value_in_pln_2(mocker):
+    mocker.patch(
+        "portfel.requester.ExchangeRateRequester.get_todays_rate", return_value=4.50
+    )
+    # pass
+    wallet_ = wallet.Wallet()
+    wallet_.add_buy_transaction(500, "2021-09-01")
+    wallet_.add_buy_transaction(200, "2021-09-02")
+
+    result = wallet_.get_value_in_pln()
+
+    assert result == 700 * 4.5
