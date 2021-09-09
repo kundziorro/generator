7bb74994ef736935e0ef71f8e1ecd0968789c720
kundiorro <grzybek2413@gmail.com>
2021-09-09 16:21:29 +0200

    new function get value in pln

--- app/wallet.py
+++ app/wallet.py
@@ -17,14 +17,15 @@ class Wallet:
         self.transactions.append(buy)
 
     def add_sell_transaction(self, value_euro, date):
-        sell = Transaction(date, value_euro)
+        sell = Transaction(date, value_euro*(-1))
+        sell = sell
         self.transactions.append(sell)
 
     def get_value_in_pln(self):
         todays_rate = self.exchange_rate_requester.get_todays_rate()
         wallet_value = 0
-        for _ in self.transactions:
-            wallet_value += Transaction[1]
+        for transaction in self.transactions:
+            wallet_value += transaction[1]
         value_in_pln = wallet_value * todays_rate
         return value_in_pln
 
