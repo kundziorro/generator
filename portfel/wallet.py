cc21f388596e0ef944bff3d8f0c61a3f5e4c12ab
kundiorro <grzybek2413@gmail.com>
2021-09-10 13:14:42 +0200

    type hints, tests

--- portfel/wallet.py
+++ portfel/wallet.py
@@ -13,16 +13,14 @@ class Wallet:
     def show_transactions(self) -> None:
         for transaction in self.transactions:
             sign = "+" if transaction.value >= 0 else ""
-            print(
-                f"In {transaction.date} {sign}{transaction.value} EUR by rate {transaction.rate}"
-            )
+            print(f"In {transaction.date} {sign}{transaction.value} EUR by rate {transaction.rate}")
 
-    def add_buy_transaction(self, value_euro, date, rate) -> None:
+    def add_buy_transaction(self, value_euro: float, date: str, rate: float) -> None:
         self.exchange_rate_requester.get_todays_rate()
         buy = Transaction(date, value_euro, rate)
         self.transactions.append(buy)
 
-    def add_sell_transaction(self, value_euro, date, rate) -> None:
+    def add_sell_transaction(self, value_euro: float, date: str, rate: float) -> None:
         sell = Transaction(date, value_euro * (-1), rate)
         self.transactions.append(sell)
 
@@ -41,6 +39,5 @@ class Wallet:
         return transactions_value_pln
 
     def get_profit_in_pln(self) -> float:
-        profit = 0
         profit = self.get_value_in_pln - self.get_paid_value_in_pln
         return profit
--- test/test_wallet.py
+++ test/test_wallet.py
@@ -18,9 +18,19 @@ def test_get_value_in_pln_2(mocker):
     )
     # pass
     wallet_ = wallet.Wallet()
-    wallet_.add_buy_transaction(500, "2021-09-01")
-    wallet_.add_buy_transaction(200, "2021-09-02")
+    wallet_.add_buy_transaction(500, "2021-09-01", 4.20)
+    wallet_.add_buy_transaction(200, "2021-09-02", 4.60)
 
     result = wallet_.get_value_in_pln()
 
     assert result == 700 * 4.5
+
+def test_add_buy_transaction():
+    wallet_ = wallet.Wallet()
+    wallet_.add_buy_transaction(500, "2021-09-01", 4.50)
+
+    result = wallet_.transactions[0]
+
+    assert result.value == 500
+    assert result.date == "2021-09-01"
+    assert result.rate == 4.5
