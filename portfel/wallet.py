51852b805f050f45296207728328ec531e602bfd
kundiorro <grzybek2413@gmail.com>
2021-09-10 12:54:42 +0200

    new functions, type hints, changes in show_transactions

--- portfel/wallet.py
+++ portfel/wallet.py
@@ -6,25 +6,27 @@ Transaction = namedtuple("Transaction", ["date", "value", "rate"])
 
 
 class Wallet:
-    def __init__(self):
+    def __init__(self) -> None:
         self.transactions = []
         self.exchange_rate_requester = ExchangeRateRequester()
 
-    def show_transactions(self):
+    def show_transactions(self) -> None:
         for transaction in self.transactions:
             sign = "+" if transaction.value >= 0 else ""
-            print(f"In {transaction.date} {sign}{transaction.value} EUR by rate {transaction.rate}")
+            print(
+                f"In {transaction.date} {sign}{transaction.value} EUR by rate {transaction.rate}"
+            )
 
-    def add_buy_transaction(self, value_euro, date, rate):
+    def add_buy_transaction(self, value_euro, date, rate) -> None:
         self.exchange_rate_requester.get_todays_rate()
         buy = Transaction(date, value_euro, rate)
         self.transactions.append(buy)
 
-    def add_sell_transaction(self, value_euro, date, rate):
+    def add_sell_transaction(self, value_euro, date, rate) -> None:
         sell = Transaction(date, value_euro * (-1), rate)
         self.transactions.append(sell)
 
-    def get_value_in_pln(self):
+    def get_value_in_pln(self) -> float:
         todays_rate = self.exchange_rate_requester.get_todays_rate()
         wallet_value = 0
         for transaction in self.transactions:
@@ -32,8 +34,13 @@ class Wallet:
         value_in_pln = wallet_value * todays_rate
         return value_in_pln
 
-    def get_paid_value_in_pln(self):
-        pass
+    def get_paid_value_in_pln(self) -> float:
+        transactions_value_pln = 0
+        for transaction in self.transactions:
+            transactions_value_pln += transaction.value * transaction.rate
+        return transactions_value_pln
 
-    def get_profit_in_pln(self):
-        pass
+    def get_profit_in_pln(self) -> float:
+        profit = 0
+        profit = self.get_value_in_pln - self.get_paid_value_in_pln
+        return profit
