03470168552e2b5746fb87e4ac3d9ad68354b779
kundziorro <grzybek2413@gmail.com>
2021-09-20 12:19:43 +0000

    tests of grapher class - in progress

--- test/test_grapher.py
+++ test/test_grapher.py
@@ -1,9 +1,15 @@
-# from app.wallet import Transaction
-# from app.visualization import Grapher
+from app.wallet import Transaction
+from app.visualization import Grapher
+import pandas as pd
 
 
-# def test_create_historical_rates_df():
-#     grapher = Grapher()
-#     transactions = Transaction("2021-09-01", 100, 4.2)
+def test_create_historical_rates_df(mocker):
+    mocker.patch(
+        "app.requester.ExchangeRateRequester.get_historical_bids", return_value={"date": "2021-09-06", "rate": 4.2}
+    )
+    grapher = Grapher()
+    transactions = Transaction("2021-09-06", 100, 4.2)
+    result = grapher._create_historical_rates_df(transactions)
+    df = pd.DataFrame({"date": "2021-09-06", "rate": 4.2})
 
-#     grapher._create_historical_rates_df(transactions)
+    assert result == df
